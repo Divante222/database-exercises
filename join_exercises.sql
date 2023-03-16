311d3cb608a8de778ec70e243d98fefac7a2a85e
Divante Parness <divante111@gmail.com>
2023-03-15 20:40:25 -0500

    looked over join_exercises.sql

--- join_exercises.sql
+++ join_exercises.sql
@@ -6,14 +6,32 @@ select * from dept_manager;
 
 select * from employees;
 
+-- Use the join_example_db. Select all the records from both the users and roles tables.
+use join_example_db;
+select * from users
+join roles on users.id = roles.id;
+/*
+Use join, left join, and right join to combine results from the users and roles tables as we did in the lesson. 
+Before you run each query, guess the expected number of results.
+*/
+select * from users
+left join roles on users.id = roles.id;
+-- 6 results
+select * from users
+right join roles on users.id = roles.id;
+-- 4 results
+
+-- Use the employees database.
+use employees;
 
 /*Using the example in the Associative Table Joins section as a guide, write a query that shows each 
 department along with the name of the current manager for that department.
 */
-select departments.dept_name, concat(employees.first_name, ' ', employees.last_name) from employees
+select departments.dept_name, concat(employees.first_name, ' ', employees.last_name) as current_manager from employees
 join dept_manager on employees.emp_no = dept_manager.emp_no
 join departments on dept_manager.dept_no = departments.dept_no
 where dept_manager.to_date like '999%';
+
 /* Find the name of all departments currently managed by women.
 */
 
@@ -24,7 +42,7 @@ where dept_manager.to_date like '999%' and
 employees.gender = 'F';
 
 -- Find the current titles of employees currently working in the Customer Service department.
-select titles.title, count(*) from employees
+select departments.dept_name, titles.title, count(*) from employees
 join titles on employees.emp_no = titles.emp_no
 join dept_emp on employees.emp_no = dept_emp.emp_no
 join departments on dept_emp.dept_no = departments.dept_no
@@ -33,12 +51,12 @@ departments.dept_no = 'd009' and
 titles.to_date like '999%'
 and departments.dept_name = 'Customer Service'
 
-group by titles.title;
+group by titles.title, departments.dept_name;
 
 
 -- Find the current salary of all current managers.
 
-select departments.dept_name, salaries.salary,concat(employees.first_name, ' ', employees.last_name) as name from employees
+select departments.dept_name, salaries.salary,concat(employees.first_name, ' ', employees.last_name) as manger_name from employees
 join dept_manager on employees.emp_no = dept_manager.emp_no
 join departments on dept_manager.dept_no = departments.dept_no
 join salaries on employees.emp_no = salaries.emp_no
@@ -52,6 +70,7 @@ join departments on dept_emp.dept_no = departments.dept_no
 where dept_emp.to_date like '999%'
 and departments.dept_name = 'Customer Service';
 -- 17569
+
 select count(*) from employees
 join dept_emp on employees.emp_no = dept_emp.emp_no
 join departments on dept_emp.dept_no = departments.dept_no
@@ -111,7 +130,9 @@ where dept_emp.to_date like '999%'
 and titles.to_date like '999%'
 and salaries.to_date like '999%'
 group by departments.dept_name 
+order by average_salary
 ;
+-- sales
 
 -- Who is the highest paid employee in the Marketing department?
 select concat(employees.first_name, ' ', employees.last_name) as name, max(salaries.salary) from employees
@@ -132,10 +153,10 @@ join departments on dept_manager.dept_no = departments.dept_no
 join salaries on employees.emp_no = salaries.emp_no
 where dept_manager.to_date like '999%'
 group by employees.first_name, employees.last_name, departments.dept_name
-order by max(salaries.salary)
+order by max(salaries.salary) desc
 ;
 
--- '106491', 'Vishwani', 'Minakawa'
+-- '106491', 'Vishwani', 'Minakawa', marketing
 
 -- Determine the average salary for each department. Use all salary information and round your results.
 select departments.dept_name, round(avg(salaries.salary),2) as average_salary from employees
