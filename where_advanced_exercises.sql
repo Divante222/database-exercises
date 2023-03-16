6d920c3ec0e154415008c4a4be123f4801495c12
Divante Parness <divante111@gmail.com>
2023-03-15 19:23:57 -0500

    updated the advanced where exercises

--- where_advanced_exercises.sql
+++ where_advanced_exercises.sql
@@ -11,7 +11,7 @@ limit 3;
 -- 10200, 10397, 10610
 
 /*Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', as in Q2, 
-but use OR instead of IN.  number of the top tWhat is the employeehree results? Does it match the previous question?*/
+but use OR instead of IN. What is the employeehree results? Does it match the previous question?*/
 
 select * from employees
 where first_name = 'Irena'
@@ -47,25 +47,27 @@ select distinct last_name from employees
 where last_name like 'E%'
 and last_name like '%e';
 
--- ########Find all current or previous employees hired in the 90s. Enter a comment with top three employee numbers.
+-- Find all current or previous employees hired in the 90s. Enter a comment with top three employee numbers.
 select * from employees
-where hire_date > '1990-01-01' 
-and hire_date < '1999-12-31'
+where hire_date like '199%'
+order by emp_no
 limit 3;
+-- 10008, 10011, 10012
 
 -- 9 Find all current or previous employees born on Christmas. Enter a comment with top three employee numbers.
 select * from employees 
 where birth_date like '%12-25'
 ;
--- Danel, Chikara, Mang
+-- 10078, 10115, 10261
  
  -- Find all current or previous employees hired in the 90s and born on Christmas. 
  -- Enter a comment with top three employee numbers.
  select * from employees
- where hire_date between '1990-01-01' and '1999-12-31'
+ where hire_date like '199%'
  and birth_date like '%12-25'
+ order by emp_no
  limit 3;
- -- mang, Evgueni, Florina
+ -- 10261, 10438, 10681
  
  -- Find all unique last names that have a 'q' in their last name.
  select distinct last_name from employees 
