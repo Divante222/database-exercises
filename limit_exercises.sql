0a96a0908d83cdc71f8f53fccac1fe20426adff9
Divante Parness <divante111@gmail.com>
2023-03-15 20:02:18 -0500

    updated the limit_exercises.sql

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,42 +1,48 @@
 use employees;
 select database();
+/*
+MySQL provides a way to return only unique results from our queries with the keyword DISTINCT. 
+For example, to find all the unique titles within the company, we could run the following query:
+SELECT DISTINCT title FROM titles;
+List the first 10 distinct """last name"""? sorted in descending order.
+*/
+
+select distinct * from titles;
+-- there is no last name in titles
+
 /*Find all previous or current employees hired in the 90s and born on Christmas. 
 Find the first 5 employees hired in the 90's by sorting by hire date and limiting your 
 results to the first 5 records. Write a comment in your code that lists the five names of the employees returned.*/
 select first_name, last_name from employees
-where hire_date between '1990-01-01' and '1999-12-31'
+where hire_date like '199%'
 and birth_date like '%12-25'
+order by hire_date
 limit 5;
 
-/* 'Mang','Erie'
-'Evgueni','Srimani'
-'Florina','Garnier'
-'Sorina','Kermarrec'
-'Chriss','Binding'
-
+/* 
+'Alselm', 'Cappello'
+'Utz', 'Mandell'
+'Bouchung', 'Schreiter'
+'Baocai', 'Kushner'
+'Petter', 'Stroustrup'
 */
 
 
 /*Try to think of your results as batches, sets, or pages. The first five results are your first page. 
 The five after that would be your second page, etc. Update the query to find the tenth page of results.*/
 select first_name, last_name from employees
-where hire_date between '1990-01-01' and '1999-12-31'
+where hire_date like '199%'
 and birth_date like '%12-25'
+order by hire_date
 limit 5
 offset 45;
 
-/* MySQL provides a way to return only unique results from our queries with the keyword DISTINCT. 
-For example, to find all the unique titles within the company, we could run the following query:*/
--- List the first 10 distinct last name sorted in descending order. 
 
-select distinct last_name from employees
-order by last_name desc
-limit 10;
 /*LIMIT and OFFSET can be used to create multiple pages of data. 
 What is the relationship between OFFSET (number of results to skip), LIMIT (number of results per page), 
 and the page number?
 */
--- (number of results per page * number of pages) -result of one page
+-- (number of results per page * number of pages) limit is the results of one page offset is the amount of pages or limit result pages you skip
 
 
 
